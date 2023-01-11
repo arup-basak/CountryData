2634fa4671cd9aaf8b62be1bc6eaa9a797fb82ba
Arup Basak <email.arupbasak@gmail.com>
2023-01-12 02:20:45 +0530

    Region Updated

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const countries_elem = document.querySelector(".countries");
+const countries_elem = document.querySelector(".container");
+
+const data = {
+  Asia: [],
+  Europe: [],
+  Africa: [],
+  Americas: [],
+  Oceania: [],
+};
 
 function getCountry(country, capital) {
-  return `<div class="country_container">
-            <span class="country_name">${country}</span>
-            <span class="country_capital">${capital}</span>
-          </div>`;
+  return `  
+    <div class="country_container">
+      <span class="country_name">${country}</span>
+      <span class="country_capital">${capital}</span>
+    </div>    
+      `;
+}
+
+function getCountries(arr) {
+  let str = "";
+  arr.forEach((elem) => {
+    str += getCountry(elem[0], elem[1]);
+  });
+  return str;
+}
+
+function getContinent(continent, countries) {
+  return `
+  <div class="continent">
+    <span class="continent_name">${continent}</span>
+    <div class="countries">
+      ${getCountries(countries)}
+    </div>
+  </div>`;
 }
 
 function arrToString(array) {
@@ -22,17 +50,21 @@ function arrToString(array) {
 async function loadJson() {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const result = await response.json();
-  console.log(result[0]);
+  // console.log(result);
 
-  let country = "",
-    capital = "";
   result.forEach((element) => {
     if (element.independent == true) {
-      country = element.name.common;
-      capital = arrToString(element.capital);
-      countries_elem.innerHTML += getCountry(country, capital);
+      data[element.region].push([
+        element.name.common,
+        arrToString(element.capital),
+      ]);
     }
   });
+
+  for (const [key, value] of Object.entries(data)) {
+    countries_elem.innerHTML += getContinent(key, value);
+    // console.log(key)
+  }
 }
 
 loadJson();
